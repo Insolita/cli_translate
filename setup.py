54ce1de3cf9d5e0122068e81c5ba05486bb4d4f5
insolita <webmaster100500@ya.ru>
2018-05-23 09:29:26 +0800

    wip

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@
 from setuptools import setup, find_packages
 import cli_translate
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name     = 'cli_translate',
     version  = cli_translate.__version__,
     packages = find_packages(),
-    requires = ['python (>= 3.0)', 'pyperclip (>= 1.5.27)', 'requests(>=2.0.1, <=3.0.0)', 'pyquery(>=1.2.9)'],
+    requires = ['python (>= 3.0)', 'pyperclip (>= 1.5.27)', 'requests(>=2.18, <=3.0.0)', 'pyquery(>=1.2.9)'],
     description  = 'Useful translation tool for console. With features: translate text from clipboard and store translation logs',
-    long_description = open('README.md').read(), 
+    long_description = long_description, 
     author       = 'Insolita',
     author_email = 'webmaster100500@ya.ru',
     url          = 'https://github.com/insolita/cli-translate',
@@ -18,8 +21,7 @@ setup(
     license      = 'MIT License',
     keywords     = 'translation, translate, yandex, google, google translate, clipboard, notify-send, console',
     classifiers  = [
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
     ],
     entry_points={
           'console_scripts': [
